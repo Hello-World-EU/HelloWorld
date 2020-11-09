700b3f935c40174cc32a14b4d8d299b83d87b399
eduardou-ub <70515731+eduardou-ub@users.noreply.github.com>
2020-11-09 18:10:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello World 1')
+  // res.status(200).json('Hello World 1')
+  res.send('Hello World 1')
 })
 
 module.exports = app.listen(4000, () => console.log(`Running on http://localhost:4000`))
