9de07b4f6199fd24792797f8bf472db3a813bfe6
eduardou-ub <70515731+eduardou-ub@users.noreply.github.com>
2020-11-09 11:56:15 +0100

    test pr
    f

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,10 +2,10 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('Get route', () => {
-  it('page should return hello world', async (done) => {
+  it('page should return hello world 1', async (done) => {
     const res = await request(app).get('/')
     expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('Hello World')
+    expect(res.body).toEqual('Hello World 1')
     done()
   })
 })
